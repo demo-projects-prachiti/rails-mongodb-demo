595bf0463ba02fd58bb6609101bab0f3790e25b7
prachiti-neosoft <prachiti.mhatre@neosoftmail.com>
2022-12-08 16:20:15 +0530

    File upload using paperclip

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -65,6 +65,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:name, :content)
+      params.require(:article).permit(:name, :content,:file)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,13 @@
 class Article
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Paperclip
+
   field :name, type: String
   field :content, type: String
+  has_mongoid_attached_file :file
+
+  validates_attachment :file, content_type: { content_type: /\Aimage\/.*\Z/ }
 
   has_many :comments, dependent: :destroy
 end
