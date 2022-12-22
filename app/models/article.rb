e692987f4e2067163c9c8ac3307cfdbff42ebc9d
prachiti-neosoft <prachiti.mhatre@neosoftmail.com>
2022-12-22 16:21:50 +0530

    Added index for name field of article

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,9 +8,20 @@ class Article
   field :content, type: String
   field :free,type: Mongoid::Boolean, default: 'true'
 
+  # index name: 'text'
+
   has_mongoid_attached_file :file
 
   validates_attachment :file, content_type: { content_type: /\Aimage\/.*\Z/ }
 
   has_many :comments, dependent: :destroy
+
+  def self.search(search)
+    if search.present?
+     Article.where('$text' => {'$search' => search }).to_a
+    else 
+      Article.all
+    end
+  end
+
 end
