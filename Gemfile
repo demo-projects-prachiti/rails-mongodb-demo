dfd65001c0ed789eb1453eed7d46ef86eb4e9293
prachiti-neosoft <prachiti.mhatre@neosoftmail.com>
2022-12-23 19:16:03 +0530

    Embeds one relation for address

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'mongoid'
 gem 'bson_ext'
 gem "mongoid-paperclip"
+gem 'devise'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
-  root "articles#index"
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
+  resources :users, controllers: { registrations: 'users/registrations' }
   resources :articles
+  root 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
